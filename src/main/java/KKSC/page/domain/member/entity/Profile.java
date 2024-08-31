dc9ab8fa88192a5a878878b24f089b7729a290c2
passionryu <rsy1225@naver.com>
2024-08-31 12:39:36 +0900

    feat: 프로필 엔티티에 사진 경로 필드 및 (인트로,닉네임,사진 경로 )정보 변경 메서드 추가

--- src/main/java/KKSC/page/domain/member/entity/Profile.java
+++ src/main/java/KKSC/page/domain/member/entity/Profile.java
@@ -2,6 +2,7 @@ package KKSC.page.domain.member.entity;
 
 import KKSC.page.domain.member.dto.request.MemberRequest;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -20,17 +21,37 @@ public class Profile {
 
     /**
      * photo 추가 예정
+     * 08.31 성열 추가 완료
      */
 
-    //자기소개
+    @Column(name = "intro", length = 200, nullable = true)
     private String intro;
 
     @Column(name = "nickname", length = 50)
     private String nickname;
 
-    public void update(MemberRequest memberRequest) {
-        this.intro = memberRequest.intro();
-        this.nickname = memberRequest.nickname();
-        //포토 추가예정
+    @Column(name = "profile_photo_path", length = 255, nullable = true)
+    private String profilePhotoPath;
+
+    // intro 변경 메서드
+    public void changeIntro(String intro) {
+        this.intro = intro;
     }
+
+    // nickname 변경 메서드
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    // profilePhotoPath 변경 메서드
+    public void changeProfilePhotoPath(String profilePhotoPath) {
+        this.profilePhotoPath = profilePhotoPath;
+    }
+
+//    //이건 아마 사용 안할것 같습니다- 성열
+//    public void update(MemberRequest memberRequest) {
+//        this.intro = memberRequest.intro();
+//        this.nickname = memberRequest.nickname();
+//        //포토 추가예정
+//    }
 }
