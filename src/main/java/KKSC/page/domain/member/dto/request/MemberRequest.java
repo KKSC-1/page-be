5abd752d1462003b251e01f5da7d6abd5ac2a125
passionryu <rsy1225@naver.com>
2024-09-10 15:33:59 +0900

    refactor :  주석 삭제

--- src/main/java/KKSC/page/domain/member/dto/request/MemberRequest.java
+++ src/main/java/KKSC/page/domain/member/dto/request/MemberRequest.java
@@ -24,14 +24,6 @@ public record MemberRequest(
         @Size(max = 20, message = "학번은 최대 20자까지 가능합니다.")
         String studentId
 
-         //no usages 여서 잠시 주석처리 -성열
-//        @Size(max = 200, message = "자기소개는 최대 200자까지 가능합니다.")
-//        String intro,
-//
-//        @Size(min = 2, max = 10, message = "별명은 최대 10자까지 가능합니다.")
-//        String nickname
-
-
 ) {
     public Member toEntity() {
         return Member.builder()
